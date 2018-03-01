7c4de6cda4e91d172c86f75d630e768547d11275
Peter MOUEZA <mouezapeter@gmail.com>
2018-03-01 12:07:04 +0100

    ssage)

--- php.php
+++ php.php
@@ -2,7 +2,7 @@
 include("personne.php");
 
 $tab = [1, 10, 20];
-$chnTab = ['f', 'o', 'o', 'bar'];
+$chnTab = ['f', 'o', 'o', 'bar','is whole'];
 
 $string = '';
 foreach ($chnTab as $value) {
@@ -13,15 +13,19 @@ echo $string;
 
 
 /* * ********************************************virgules */
+echo '<br>********************************************virgules';
+  $string = "";
 //for ($cpt = 0; $cpt < $chnTab . length - 1;) {
 for ($cpt = 0; $cpt < count($chnTab) - 1;$cpt++) {
-    echo "cpt=" . $cpt;
+    echo "cpt=" . $cpt."<br>";
     //$string += $value;
+    $value=$chnTab[$cpt];
     $string = $string . $value . ",";
 }
 
 //$string += $chnTab[$chnTab . length - 1];
-$string += $chnTab[count($chnTab) - 1];
+//$string += $chnTab[count($chnTab) - 1];
+$string = $string . $chnTab[count($chnTab) - 1];
 
 echo $string;
 
@@ -32,4 +36,7 @@ echo $string;
 
 /* * Abstract Constructor  Inheritance */
 $personne = new Personne();
-$personne.setName("MOUEZA");
+$personne->setName("MOUEZA");
+$name = $personne->getName();
+echo "".$name;
+
